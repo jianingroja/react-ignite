539eee1a673699043ce0db384660c0938361eab8
Jianing <jianingw321@outlook.com>
2020-11-14 10:01:07 +0800

    9. Style Cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import { useEffect } from "react";
 //Redux
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../action/gamesAction";
+//Components
+import Game from "./components/Game";
+//Style and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   //Fetch Games
@@ -9,13 +14,43 @@ const Home = () => {
   useEffect(() => {
     dispatch(loadGames());
     console.log("hi");
-  });
+  }, [dispatch]);
 
+  //Get data from state
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+  console.log(newGames);
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            key={game.id}
+            id={game.id}
+            image={game.background_image}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  //auto fit columns in a row, minimun 500px, otherwise take as much as left
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
