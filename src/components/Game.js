b3e67873df9b7eb1c91fb7a109173ae7dbda1c54
Jianing <jianingw321@outlook.com>
2020-11-14 10:59:51 +0800

    11. Fetch Game Details

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,10 +2,18 @@
 //Style and Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
+//Redux
+import { useDispatch } from "react-redux";
+import loadDetail from "../action/detailAction";
 
 const Game = ({ name, released, id, image }) => {
+  //Load Details
+  const dispatch = useDispatch();
+  const loadDetailHandler = () => {
+    dispatch(loadDetail(id));
+  };
   return (
-    <StyledGame>
+    <StyledGame onClick={loadDetailHandler}>
       <h3>{name}</h3>
       <p>{released}</p>
       <img src={image} alt={name} />
