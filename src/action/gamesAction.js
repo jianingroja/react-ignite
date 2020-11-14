fb1cdd9c5a32c5707f104c284ede2a40cf4ef2b0
Jianing <jianingw321@outlook.com>
2020-11-14 09:27:08 +0800

    7. Fetch other games data

--- src/action/gamesAction.js
+++ src/action/gamesAction.js
@@ -1,17 +1,19 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from "../api";
 
 //Action Creator
 
 export const loadGames = () => async (dispatch) => {
-  //Fetch Sxios
+  //Fetch Axios
   const popularData = await axios.get(popularGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
+  const upcomingData = await axios.get(upcomingGamesURL());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      newGames: newGamesData.data.results,
+      upcoming: upcomingData.data.results,
     },
   });
-
-  return;
 };
